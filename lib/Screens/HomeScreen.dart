873ff7afe9ee43243fc7c6ba711d8d71d768cd4b
RotenKiwi <joshiparth212@gmail.com>
2023-05-21 00:48:30 +0530

    Weekday
    function to return weekday name

--- lib/Screens/HomeScreen.dart
+++ lib/Screens/HomeScreen.dart
@@ -1,10 +1,16 @@
 import 'package:flutter/material.dart';
+import 'package:tale_hub/Tools/weekday.dart';
 
 class Home extends StatelessWidget {
   const Home({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    var day = DateTime.now().weekday;
+    return Column(
+      children: [
+        Text(weekday(day))
+      ],
+    );
   }
 }
