46e7a3d9e80c191a8052041f0bfaf80a4cdba0e5
RotenKiwi <joshiparth212@gmail.com>
2023-05-21 19:04:45 +0530

    LoginPage
    Design changes to Login Page

--- lib/Components/formField.dart
+++ lib/Components/formField.dart
@@ -2,24 +2,30 @@ import 'package:flutter/material.dart';
 
 class formField extends StatelessWidget {
   final String text;
+
   //final Color textColor;
   final double horizontalPadding;
-  final double maxWidth;
+  final double fontSize;
+  final Function(String) onChanged;
+  final bool password;
 
   const formField({
     super.key,
     required this.text,
     //required this.textColor,
     required this.horizontalPadding,
-    required this.maxWidth,
+    required this.fontSize,
+    required this.onChanged, required this.password,
   });
 
   @override
   Widget build(BuildContext context) {
     return Padding(
       padding:
-      EdgeInsets.symmetric(horizontal: horizontalPadding, vertical: 15),
+          EdgeInsets.symmetric(horizontal: horizontalPadding, vertical: 15),
       child: TextFormField(
+        obscureText: password,
+        onChanged: onChanged,
         decoration: InputDecoration(
           border: const OutlineInputBorder(
             borderRadius: BorderRadius.all(
@@ -29,8 +35,8 @@ class formField extends StatelessWidget {
           hintText: text,
           hintStyle: TextStyle(
             //color: textColor,
-            color: Colors.black,
-            fontSize: maxWidth * 0.04,
+            color: Colors.white,
+            fontSize: fontSize,
             fontFamily: 'Poppins',
           ),
         ),
--- lib/Screens/LoginSignup/Login.dart
+++ lib/Screens/LoginSignup/Login.dart
@@ -1,6 +1,7 @@
 import 'dart:ui';
 
 import 'package:flutter/material.dart';
+import 'package:tale_hub/Components/formField.dart';
 
 class login extends StatefulWidget {
   const login({Key? key}) : super(key: key);
@@ -12,28 +13,56 @@ class login extends StatefulWidget {
 class _loginState extends State<login> {
   @override
   Widget build(BuildContext context) {
-    return Container(
-      decoration: BoxDecoration(
-        image: DecorationImage(
-          image: AssetImage('assets/images/loginSignup(2).jpg'),
-            colorFilter: ColorFilter.mode(Colors.grey, BlendMode.modulate),
-            fit: BoxFit.cover),
+    return Scaffold(
+      body: Container(
+        decoration: const BoxDecoration(
+          image: DecorationImage(
+              image: AssetImage('assets/images/loginSignup(2).jpg'),
+              colorFilter: ColorFilter.mode(Colors.grey, BlendMode.modulate),
+              fit: BoxFit.cover),
         ),
-      child: Column(
-        children: [
-          Text("HI"),
-          ClipRRect(
-            child: BackdropFilter(
-              filter: ImageFilter.blur(sigmaX: 10, sigmaY: 10),
-              child: Column(
-                children: [
-
-                ],
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            const Spacer(),
+            const Padding(
+              padding: EdgeInsets.symmetric(horizontal: 40.0),
+              child: Text(
+                "HI !",
+                style: TextStyle(
+                  fontSize: 60,
+                  fontFamily: 'Breath',
+                  decoration: TextDecoration.none,
+                  color: Colors.white,
+                ),
+                textAlign: TextAlign.start,
               ),
             ),
-          ),
-        ]
+            ClipRRect(
+              child: BackdropFilter(
+                filter: ImageFilter.blur(sigmaX: 10, sigmaY: 10),
+                child: Column(
+                  children: [
+                    formField(
+                      text: 'Email',
+                      horizontalPadding: 10.0,
+                      fontSize: 25,
+                      onChanged: (String) {},
+                      password: false,
+                    ),
+                    formField(
+                        text: 'Password',
+                        horizontalPadding: 10.0,
+                        fontSize: 25,
+                        onChanged: (String) {},
+                        password: true)
+                  ],
+                ),
+              ),
+            ),
+          ],
+        ),
       ),
-      );
+    );
   }
 }
