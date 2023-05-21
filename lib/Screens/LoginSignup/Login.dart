8777ae2ad22bdaf34fd9ab312be46fa0ba07bd90
RotenKiwi <joshiparth212@gmail.com>
2023-05-21 19:39:38 +0530

    LoginPage
    Completed designing LoginPage

--- lib/Screens/LoginSignup/Login.dart
+++ lib/Screens/LoginSignup/Login.dart
@@ -1,6 +1,7 @@
 import 'dart:ui';
 
 import 'package:flutter/material.dart';
+import 'package:tale_hub/Components/RoundedButton.dart';
 import 'package:tale_hub/Components/formField.dart';
 
 class login extends StatefulWidget {
@@ -24,30 +25,105 @@ class _loginState extends State<login> {
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.start,
           children: [
-            const Spacer(),
+            const Spacer(
+              flex: 3,
+            ),
             const Padding(
               padding: EdgeInsets.symmetric(horizontal: 30.0),
               child: Text(
                 "HI !",
                 style: TextStyle(
-                  fontSize: 60,
+                  fontSize: 65,
                   fontFamily: 'Breath',
                   decoration: TextDecoration.none,
+                  fontWeight: FontWeight.w600,
                   color: Colors.white,
                 ),
                 textAlign: TextAlign.start,
               ),
             ),
-            ClipRRect(
-              child: BackdropFilter(
-                filter: ImageFilter.blur(sigmaX: 15, sigmaY: 15),
-                child: Column(
-                  children: [
-                    textfield(text: 'Email', isPassword: false, onchanged: (value){}),
-                    textfield(text: 'Password', isPassword: true, onchanged: (value){}),
-                  ],
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 10.0),
+              child: ClipRRect(
+                borderRadius: const BorderRadius.all(Radius.circular(20.0)),
+                child: BackdropFilter(
+                  filter: ImageFilter.blur(sigmaX: 20, sigmaY: 20),
+                  child: Padding(
+                    padding: const EdgeInsets.symmetric(vertical: 10.0),
+                    child: Column(
+                      //crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        textfield(
+                            text: 'Email',
+                            isPassword: false,
+                            onchanged: (value) {}),
+                        textfield(
+                            text: 'Password',
+                            isPassword: true,
+                            onchanged: (value) {}),
+                        const SizedBox(
+                          height: 20,
+                        ),
+                        RoundedButton(
+                            text: 'Continue',
+                            press: () {},
+                            color: Colors.greenAccent,
+                            textColor: Colors.white,
+                            length: 0.9),
+                        SizedBox(
+                          height: 30,
+                        ),
+                        Row(
+                          children: [
+                            const Text(
+                              "Don't Have an Account?   ",
+                              style: TextStyle(
+                                fontSize: 20,
+                                fontFamily: 'Breath',
+                                decoration: TextDecoration.none,
+                                color: Colors.white,
+                              ),
+                              textAlign: TextAlign.start,
+                            ),
+                            TextButton(
+                              onPressed: () {},
+                              child: const Text(
+                                "Sign Up",
+                                style: TextStyle(
+                                  fontSize: 20,
+                                  fontFamily: 'Breath',
+                                  decoration: TextDecoration.none,
+                                  color: Colors.greenAccent,
+                                ),
+                                textAlign: TextAlign.start,
+                              ),
+                            ),
+                          ],
+                        ),
+                        SizedBox(
+                          height: 20,
+                        ),
+                        // TextButton(
+                        //   onPressed: () {},
+                        //   child: const Text(
+                        //     "Forgot Password?",
+                        //     style: TextStyle(
+                        //       fontSize: 20,
+                        //       fontFamily: 'Breath',
+                        //       decoration: TextDecoration.none,
+                        //       color: Colors.greenAccent,
+                        //     ),
+                        //     textAlign: TextAlign.start,
+                        //   ),
+                        // ),
+                      ],
+                    ),
+                  ),
                 ),
               ),
+            ),
+            Spacer(
+              flex: 1,
             ),
           ],
         ),
